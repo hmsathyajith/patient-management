fddeeb377a711e12aed4d91eca51ed9f61ac5874
Sathyajith Saliya <hmsathyajith@gmail.com>
2025-08-31 12:33:42 +0530

    commit - patient service

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a complete microservices project using Spring boot technologies. Finally this will deoply to a AWS server. 
+As the first step we will create the patient service. 
+We have used intelij's built in Http Request feature to test the end point. 
---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,24 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+    private PatientRepository  patientRepository;
+
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        //List<PatientResponseDTO> patientResponseDTOS = patients.stream().map(PatientMapper::toDTO).toList(); <- method reference way
+        return patients.stream().map(patient -> (PatientMapper.toDTO(patient))).toList();
+    }
+}
